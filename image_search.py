a3a1ea90bb602626e7ec29bce5d21ccfbe662397
Nick_PC <ndbarrett63@gmail.com>
2021-01-20 10:50:47 -0500

    Update image_search.py

--- image_search.py
+++ image_search.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 client = discord.Client()
 load_dotenv()
 discord_key = os.getenv('image_search_key')
+driver = webdriver.Firefox()
 
 @client.event
 async def on_message(message):
@@ -22,12 +23,12 @@ async def on_message(message):
         search_terms = message.content.split('~search ')[-1].replace(' ', '+')
         url = "https://duckduckgo.com/?q={}&iar=images&iax=images&ia=images".format(search_terms)
         class_name = "tile--img__img"
-        driver = webdriver.Firefox()
+        # driver = webdriver.Firefox()
         driver.get(url)
         element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
         src = element.get_attribute('src')
         await message.channel.send(src)
-        driver.close()
+        # driver.close()
 
 client.run(discord_key)
 
